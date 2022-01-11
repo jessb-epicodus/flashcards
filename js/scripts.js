80178b97e48194cc70855f3e184bb4964d7e71ba
jb <jessb.epicodus@gmail.com>
2022-01-11 15:06:27 -0800

    WIP - add clickable text

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#Javascript-shown").toggle();
+    $("#Javascript-hidden").toggle();
+  });
+
+$(".clickable").click(function() {
+  $("#Variables-shown").toggle();
+  $("#Variables-hidden").toggle();
+});
+});
